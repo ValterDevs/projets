f7f05fb4a87c2aa2bc9dc06ba3aa8ea28683cda2
ValterDevs <135986673+ValterDevs@users.noreply.github.com>
2024-08-22 19:05:26 -0300

    Create calculing_investment_python.py
    I created a code for a fixed income investment calculator
    it´s my first project

---
+++ calculing_investment_python.py
@@ -0,0 +1,15 @@
+#calculating a fixed income investment with Python
+#formule : m=c(1+i)**n
+#m= value c= initial capital n= time (month) i=rate
+c = int(input("write the investment initial:"))
+i = int(input("write the interest rate:"))
+n = int(input("write the time (in the month):"))
+
+j = i/100             
+a= (1+j)**n           
+b= c*a
+l= b *0.1448
+
+
+print(f'value liquid: {b:.2f}')
+print(f'value of govern receive(tax): {l:.2f}')
